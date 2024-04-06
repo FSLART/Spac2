bd077c055d7213857d6a1c9d3895148df371d130
guilhermembp <2211062@my.ipleiria.pt>
2024-04-06 16:35:17 +0100

    new tests for the 2 quadrants that weren't tested before

--- spac2_0/test/tst_target.cpp
+++ spac2_0/test/tst_target.cpp
@@ -84,11 +84,37 @@ TEST (tst_target, get_angle_invalid_path){
     ASSERT_EQ(TWOCUTFLOATING(steering_angle), TWOCUTFLOATING(expected_steering_angle));
 }
 
+TEST (tst_target, get_angle_invalid_path_negative_y){
+    nav_msgs::msg::Path path;
+    //current_rpm influences the lookahed distance that influences the steering angle
+    int cureent_rpm = 400;      //current speed: 2.67 m/s (9,61 km/h)
+    float expected_steering_angle = 0.0;
+    geometry_msgs::msg::Point point;
+    geometry_msgs::msg::Quaternion quaternion;
+    point.x = 50.0;
+    point.y = -50.0;
+    point.z = 0.0;
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = 0.0;
+    quaternion.w = 1.0;
+    geometry_msgs::msg::Pose pose;
+    pose.position = point;
+    pose.orientation = quaternion;
+    geometry_msgs::msg::PoseStamped pose_stamped;
+    pose_stamped.pose = pose;
+    path.poses.push_back(pose_stamped);
+    Target target(417);
+    float steering_angle = target.get_steering_angle(path, cureent_rpm);
+    //std::cerr << "[          ] steering angle = " << steering_angle << std::endl;
+    ASSERT_EQ(TWOCUTFLOATING(steering_angle), TWOCUTFLOATING(expected_steering_angle));
+}
+
 TEST (tst_target, get_angle_3_points){
     nav_msgs::msg::Path path;
     //current_rpm influences the lookahed distance that influences the steering angle
     int cureent_rpm = 400;      //current speed: 2.67 m/s (9,61 km/h)
-    float expected_steering_angle = 44.820065635;   //TODO: the output from the program is 44.24...  Problably just because of some rounding error
+    float expected_steering_angle = 44.820065635;   
     geometry_msgs::msg::Point pointM;
     geometry_msgs::msg::Point pointN;
     geometry_msgs::msg::Point pointO;
@@ -134,5 +160,58 @@ TEST (tst_target, get_angle_3_points){
     Target target(417);
     float steering_angle = target.get_steering_angle(path, cureent_rpm);
     //std::cerr << "[          ] steering angle = " << steering_angle << std::endl;
-    ASSERT_EQ(TWOCUTFLOATING(steering_angle), TWOCUTFLOATING(expected_steering_angle));
+    ASSERT_EQ((int)steering_angle, (int) expected_steering_angle);
+}
+
+TEST (tst_target, get_angle_left_3_points){
+    nav_msgs::msg::Path path;
+    //current_rpm influences the lookahed distance that influences the steering angle
+    int cureent_rpm = 400;      //current speed: 2.67 m/s (9,61 km/h)
+    float expected_steering_angle = 48.820065635;   //TODO: the calculations for this value are not done yet
+    geometry_msgs::msg::Point pointM;
+    geometry_msgs::msg::Point pointN;
+    geometry_msgs::msg::Point pointO;
+    
+    geometry_msgs::msg::Quaternion quaternion;
+    pointM.x = 0.4;
+    pointM.y = 0.45;
+    pointM.z = 0.0; //irrelevant
+    
+    pointN.x = 0.7;
+    pointN.y = 1.0;
+    pointN.z = 0.0; //irrelevant
+    
+    pointO.x = -1;
+    pointO.y = 2;
+    pointO.z = 0.0; //irrelevant
+    
+    quaternion.x = 0.0;
+    quaternion.y = 0.0;
+    quaternion.z = 0.0;
+    quaternion.w = 1.0;
+    geometry_msgs::msg::Pose poseM;
+    poseM.position = pointM;
+    poseM.orientation = quaternion;
+    geometry_msgs::msg::PoseStamped pose_stampedM;
+    pose_stampedM.pose = poseM;
+    path.poses.push_back(pose_stampedM);
+
+    geometry_msgs::msg::Pose poseN;
+    poseN.position = pointN;
+    poseN.orientation = quaternion; //quaternion is irrelevant so it is the same var as the previous one
+    geometry_msgs::msg::PoseStamped pose_stampedN;
+    pose_stampedN.pose = poseN;
+    path.poses.push_back(pose_stampedN);
+
+    geometry_msgs::msg::Pose poseO;
+    poseO.position = pointO;
+    poseO.orientation = quaternion;
+    geometry_msgs::msg::PoseStamped pose_stampedO;
+    pose_stampedO.pose = poseO;
+    path.poses.push_back(pose_stampedO);
+
+    Target target(417);
+    float steering_angle = target.get_steering_angle(path, cureent_rpm);
+    //std::cerr << "[          ] steering angle = " << steering_angle << std::endl;
+    ASSERT_EQ((int)steering_angle, (int) expected_steering_angle);
 }
