a74b340e5ee1a3f884df093accfc2ffb4d77e049
PlagySantos <tomasmarcelinosantos@gmail.com>
2025-07-18 19:53:59 +0100

    testing

--- spac2_0/src/fmath.cpp
+++ spac2_0/src/fmath.cpp
@@ -78,7 +78,7 @@ float Pure_Pursuit::calculate_steering_angle(lart_msgs::msg::PathSpline path, fl
 
     
     // RCLCPP_INFO(rclcpp::get_logger("pure"),"Pose: x=%f y=%f",this->current_pose.position.x,this->current_pose.position.y);
-    // RCLCPP_INFO(rclcpp::get_logger("pure"),"Heading = %f",this->current_pose.orientation.w);
+    RCLCPP_INFO(rclcpp::get_logger("pure"),"Heading = %f",this->current_pose.orientation.w);
     RCLCPP_INFO(rclcpp::get_logger("pure"),"New Paths");
     for (long unsigned int i = 0; i < path.poses.size(); i++)
     {
